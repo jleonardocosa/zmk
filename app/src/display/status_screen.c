112d06bfc9e4249c15bf5ded03e9b045464bed1a
hopg <glen.hopkins04@gmail.com>
2022-07-03 21:31:49 +0100

    lily58 oled hello world test and sofle encoder

--- app/src/display/status_screen.c
+++ app/src/display/status_screen.c
@@ -29,20 +29,36 @@ static struct zmk_widget_layer_status layer_status_widget;
 static struct zmk_widget_wpm_status wpm_status_widget;
 #endif
 
+
+
 lv_obj_t *zmk_display_status_screen() {
     lv_obj_t *screen;
 
     screen = lv_obj_create(NULL, NULL);
+    
+#if IS_ENABLED(CONFIG_SSD1306_REVERSE_MODE)
+    lv_obj_t *label1 = lv_label_create(screen, NULL);
+    lv_label_set_text(label1, "hello world");
+    lv_obj_align(label1, NULL, LV_ALIGN_CENTER, 0, 0);
+#endif
+
 
 #if IS_ENABLED(CONFIG_ZMK_WIDGET_BATTERY_STATUS)
     zmk_widget_battery_status_init(&battery_status_widget, screen);
     lv_obj_align(zmk_widget_battery_status_obj(&battery_status_widget), NULL, LV_ALIGN_IN_TOP_RIGHT,
                  0, 0);
+    if (CONFIG_SHIELD_LILY58_RIGHT)
+        lv_obj_align(zmk_widget_battery_status_obj(&battery_status_widget), NULL, LV_ALIGN_IN_TOP_LEFT,
+                 0, 0);
+    
 #endif
 
 #if IS_ENABLED(CONFIG_ZMK_WIDGET_OUTPUT_STATUS)
     zmk_widget_output_status_init(&output_status_widget, screen);
     lv_obj_align(zmk_widget_output_status_obj(&output_status_widget), NULL, LV_ALIGN_IN_TOP_LEFT, 0,
+                 0);
+    if (CONFIG_SHIELD_LILY58_RIGHT)
+        lv_obj_align(zmk_widget_output_status_obj(&output_status_widget), NULL, LV_ALIGN_IN_TOP_RIGHT, 0,
                  0);
 #endif
 
@@ -51,13 +67,13 @@ lv_obj_t *zmk_display_status_screen() {
     lv_obj_set_style_local_text_font(zmk_widget_layer_status_obj(&layer_status_widget),
                                      LV_LABEL_PART_MAIN, LV_STATE_DEFAULT,
                                      lv_theme_get_font_small());
-    lv_obj_align(zmk_widget_layer_status_obj(&layer_status_widget), NULL, LV_ALIGN_IN_BOTTOM_LEFT,
+    lv_obj_align(zmk_widget_layer_status_obj(&layer_status_widget), NULL, LV_ALIGN_CENTER,
                  0, 0);
 #endif
 
 #if IS_ENABLED(CONFIG_ZMK_WIDGET_WPM_STATUS)
     zmk_widget_wpm_status_init(&wpm_status_widget, screen);
-    lv_obj_align(zmk_widget_wpm_status_obj(&wpm_status_widget), NULL, LV_ALIGN_IN_BOTTOM_RIGHT, -12,
+    lv_obj_align(zmk_widget_wpm_status_obj(&wpm_status_widget), NULL, LV_ALIGN_IN_BOTTOM_LEFT, -15,
                  0);
 #endif
     return screen;
